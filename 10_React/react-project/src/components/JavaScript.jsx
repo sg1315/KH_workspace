69c5998d77641d634551c722604d3f4c0080d415
sg135 <tropicana1235@gmail.com>
2025-04-17 15:00:39 +0900

    .

--- 10_React/react-project/src/components/JavaScript.jsx
+++ 10_React/react-project/src/components/JavaScript.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+const text = "Hello. JSX"
+const num = 10;
+
 const JavaScript = () => {
   return (
-    <div>JavaScript</div>
+    <div>
+        <h2>변수 표현 방법</h2>
+        <ul>
+            <li>{text}</li>
+            <li>{text + " TEST"}</li>
+        </ul>
+        <h2>숫자및 계산식 사용</h2>
+        <ul>
+            <li>{num}</li>
+            <li>{num + 20}</li>
+        </ul>
+
+        <h2>Boolean 값</h2>
+        <ul>
+            <li>{true}</li>
+            <li>{false}</li>
+            <li>{undefined}</li>
+            <li>{null}</li>
+        </ul>
+
+        <h2></h2>
+    </div>
   )
 }
 
--- 10_React/react-project/src/main.jsx
+++ 10_React/react-project/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import JavaScript from './components/JavaScript.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <JavaScript />
 )
