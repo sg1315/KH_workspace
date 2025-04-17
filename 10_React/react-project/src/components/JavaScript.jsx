96206d6d00f1a62b86202883f29197dbe87a5edf
sg135 <tropicana1235@gmail.com>
2025-04-17 15:35:44 +0900

    .

--- 10_React/react-project/src/components/JavaScript.jsx
+++ 10_React/react-project/src/components/JavaScript.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
+const imgUrl = "https://i.namu.wiki/i/ZbNPpk4fG_ZEd7jf7aiYyRhqbXqocxfeSWhxwvBP9YeBn09fLGXbCQBaSGBN7QuZ-0En6Tu25OlUwGL79KqYYEC_9yw42m3IZmFH9LpJ2HiSN6UMmRz8YrJuMy4Lgf5bAdlyA3SoIe1mn1SESCXSsw.webp"
 const text = "Hello. JSX"
 const num = 10;
+
+const loginUser = {
+    name : "aaa",
+    id : "idid",
+    age : "27"
+}
+
+function getImg(){
+    return <img src={imgUrl} alt="빵빵이" width={100} />
+}
+
+const numbers = [1, 2, 3]
 
 const JavaScript = () => {
   return (
@@ -25,7 +38,37 @@ const JavaScript = () => {
             <li>{null}</li>
         </ul>
 
-        <h2></h2>
+        <h2>Object, Array 값</h2>
+        <ul>
+            {
+                Object.keys(loginUser).map((key) => 
+                <li key={key}>  {/* 반복되는 요소안에는 무조건 key라는 속성이 필요함 */}
+                    {key} : {loginUser[key]}
+                </li>
+                )
+            }
+            <li>{numbers}</li> {/* 배열 그대로 출력(문자열로 자동 변환) */}
+            {[<li key={1}>111</li>, <li key={2}>222</li>, <li key={3}>333</li>]}
+        </ul>
+
+        <h2>태그 속성에 값 넣기</h2>
+        <ul>
+            <li>
+                <img src={imgUrl} alt="빵빵이" width={100} />
+            </li>
+        </ul>
+
+        <h2>조건부 렌더링</h2>
+        <ul>
+            <li>{num >  10 ? "10보다 큼" : "10보다 작음"}</li>
+            <li>{num != 10 && "조건이 true일때 안보임"}</li>
+            <li>{num > 10 || "조건이 true일때 보임"}</li>
+        </ul>
+
+        <h2>함수호출로 태그가 올 수 있음</h2>
+        <ul>
+            <li>{getImg()}</li>
+        </ul>
     </div>
   )
 }
